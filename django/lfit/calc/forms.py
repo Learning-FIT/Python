452a3232f81554824bec3f83cdee6789d53d70ef
Inoue, Kenichi <inoue@vivinko.com>
2022-02-01 17:59:53 +0900

    day6-2

--- django/lfit/calc/forms.py
+++ django/lfit/calc/forms.py
@@ -12,3 +12,10 @@ class ItemForm(forms.ModelForm):
 class ItemSearchForm(forms.Form):
     code = forms.CharField(label='商品コード', required=False)
     name = forms.CharField(label='品名', required=False)
+
+    price_min = forms.IntegerField(label='単価（下限）')
+    price_max = forms.IntegerField(label='単価（上限）')
+
+    CHOICES = [('and', 'AND条件で検索'), ('or', 'OR条件で検索')]
+    condition = forms.ChoiceField(
+        choices=CHOICES, widget=forms.RadioSelect, initial='and')
--- django/lfit/calc/views.py
+++ django/lfit/calc/views.py
@@ -1,6 +1,7 @@
 from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
+from django.db.models import Q
 from .models import *
 from .forms import *
 
@@ -57,15 +58,35 @@ def index(request):
         form = ItemSearchForm(request.POST)
         if form.is_valid():
             #items = Item.objects.filter(code=form.cleaned_data['code'])
-            if form.cleaned_data['code'] == '' and form.cleaned_data['name'] == '':
-                message = '商品コードか品名のどちらかを入力してください'
+            if form.cleaned_data['code'] == '' and form.cleaned_data['name'] == '' and form.cleaned_data['price_min'] == '' and form.cleaned_data['price_max'] == '':
+                message = '商品コードか品名か価格のいずれかを入力してください'
             else:
                 items = Item.objects
-                if form.cleaned_data['code'] != '':
-                    items = items.filter(code=form.cleaned_data['code'])
-                if form.cleaned_data['name'] != '':
-                    items = items.filter(
-                        name__contains=form.cleaned_data['name'])
+                if form.cleaned_data['condition'] == 'and':
+                    if form.cleaned_data['code'] != '':
+                        items = items.filter(code=form.cleaned_data['code'])
+                    if form.cleaned_data['name'] != '':
+                        items = items.filter(
+                            name__contains=form.cleaned_data['name'])
+                    if form.cleaned_data['price_min'] != '':
+                        items = items.filter(price__gte=int(
+                            form.cleaned_data['price_min']))
+                    if form.cleaned_data['price_max'] != '':
+                        items = items.filter(price__lte=int(
+                            form.cleaned_data['price_max']))
+                else:
+                    q_obj = Q()
+                    if form.cleaned_data['code'] != '':
+                        q_obj.add(Q(code=form.cleaned_data['code']), Q.OR)
+                    if form.cleaned_data['name'] != '':
+                        q_obj.add(
+                            Q(name__contains=form.cleaned_data['name']), Q.OR)
+                    if form.cleaned_data['price_min'] != '':
+                        items = items.filter(price__gte=int(
+                            form.cleaned_data['price_min']))
+                    if form.cleaned_data['price_max'] != '':
+                        items = items.filter(price__lte=int(
+                            form.cleaned_data['price_max']))
                 if len(items) < 1:
                     message = '商品が見つかりません'
     else:
