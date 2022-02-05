c51fb8fc09919c821001275aa5e65bd34e00e177
Inoue, Kenichi <inoue@vivinko.com>
2022-02-05 18:03:19 +0900

    day6

--- django/lfit/lfit/urls.py
+++ django/lfit/lfit/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('hello/', include('hello.urls')),
     path('calc/', include('calc.urls')),
     path('admin/', admin.site.urls),
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
