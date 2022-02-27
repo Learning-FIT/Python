f908921942ecf88a126954c284e908758dcf38ac
Kenichi Inoue <inoue@vivinko.com>
2022-02-27 18:11:26 +0900

    課題分割

--- django/lfit/calc/forms.py
+++ django/lfit/calc/forms.py
@@ -1,24 +1,17 @@
+from dataclasses import fields
 from django import forms
 from .models import *
 
-
 class ItemForm(forms.ModelForm):
-    #image = forms.ImageField(required=False)
-
     class Meta:
         model = Item
-        fields = ['code', 'name', 'price', 'image']
+        fields = ['code', 'name', 'price']
 
+# Day6 商品検索の作成
 class ItemSearchForm(forms.Form):
     code = forms.CharField(label='商品コード', required=False)
     name = forms.CharField(label='品名', required=False)
 
-    price_min = forms.IntegerField(label='単価（下限）', required=False)
-    price_max = forms.IntegerField(label='単価（上限）', required=False)
-
-    CHOICES = [('and', 'AND条件で検索'), ('or', 'OR条件で検索')]
-    condition = forms.ChoiceField(
-        choices=CHOICES, widget=forms.RadioSelect, initial='and')
-
+# Day6 明細登録画面の作成
 class LineForm(forms.Form):
     count = forms.IntegerField(label='個数', required=True)
