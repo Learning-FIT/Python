d9351834c9c72c3887765ece03b9fea53fc5710a
Kenichi Inoue <inoue@vivinko.com>
2022-02-28 19:30:16 +0900

    day7 django

--- django/lfit/calc/urls.py
+++ django/lfit/calc/urls.py
@@ -12,5 +12,11 @@ urlpatterns = [
     # Day6 ナビゲーションバー
     path('line/<int:item_id>', views.line, name='line'),
     path('cart/', views.cart, name='cart'),
-
+    # Day6 ショッピングカートを空にする、注文
+    path('cart_clear/', views.cart_clear, name='cart_clear'),
+    path('save_order/', views.save_order, name='save_order'),
+    # Day7 API
+    path('items.json', views.items_json, name='items_json'),
+    # 課題（11）
+    path('orders.json', views.orders_json, name='orders_json'),
 ]
