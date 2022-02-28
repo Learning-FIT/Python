e96b09aa5c15f788b2c13d2597826fbcde0450c4
Kenichi Inoue <inoue@vivinko.com>
2022-02-28 12:43:25 +0900

    Update views_kadai9.py

--- django/lfit/calc/views_kadai9.py
+++ django/lfit/calc/views_kadai9.py
@@ -63,7 +63,7 @@ def index(request):
             if form.cleaned_data['code'] == '' and form.cleaned_data['name'] == '' and form.cleaned_data['price_min'] is None and form.cleaned_data['price_max'] is None:
                 messages.error(request, '商品コードか品名か価格のいずれかを入力してください')
             else:
-                items = Item.objects.all()
+                items = Item.objects
                 if form.cleaned_data['condition'] == 'and':
                     if form.cleaned_data['code'] != '':
                         items = items.filter(code=form.cleaned_data['code'])
@@ -78,8 +78,7 @@ def index(request):
                     if form.cleaned_data['code'] != '':
                         q_obj.add(Q(code=form.cleaned_data['code']), Q.OR)
                     if form.cleaned_data['name'] != '':
-                        q_obj.add(
-                            Q(name__contains=form.cleaned_data['name']), Q.OR)
+                        q_obj.add(Q(name__contains=form.cleaned_data['name']), Q.OR)
                     q_price = Q()
                     if form.cleaned_data['price_min'] != None:
                         q_price.add(Q(price__gte=int(form.cleaned_data['price_min'])), Q.AND)
